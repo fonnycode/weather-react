53bfd1044f04f3eab3b1425bd69f6c7ccaee720f
fonnycode <bf.tinydiaries@gmail.com>
2024-01-11 20:30:47 -0500

    completed the search engine and includes bootstraps css .

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Search() {
-    let [message, setMessage] = useState(false);
-  let [city, setCity] = useState("");
-  let [weather, setWeather] = useState({}); 
+  let [city, setCity] = useState("Montreal");
+  let [weather, setWeather] = useState({});
+   let [content, setContent] = useState(""); 
  
   function displayWeather(response){
-    setMessage(true);
+    setContent("true");
     setWeather({
-        temperature: Math.round(response.data.main.temp),
-        description: response.data.weather[0].description,
-        wind: Math.round (response.data.wind.speed),
-        humidity: Math.round(response.data.main.humidity),
-
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: Math.round(response.data.wind.speed),
+      humidity: Math.round(response.data.main.humidity),
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
@@ -31,20 +31,59 @@ function updateCity(event){
 }
 let form =(
     <form onSubmit={handleSubmit}>
-        <input type="search" placeholder="Search a city" onChange={updateCity} />
-        <input type="submit" value="Search" />
+        <input type="search" className="w-75 p-2 me-2 rounded border-1" placeholder="Search a city" onChange={updateCity} />
+        <input type="submit" value="Search" className="p-2 border-0 rounded " />
     </form>
 );
-if (message){
+
+let heading = (
+  <div className="main">
+    <h1 className="cities ms-5 mt-5 text-capitalize"> {city} </h1>
+    <small className="ms-5">Last updated: Thursday 20:00</small>
+  </div>
+);
+
+let element = (
+  <div className="temperature container">
+    <div className="row mt-5">
+      <div className="col-4 mt-3">
+        <strong className="degrees">{weather.temperature}°</strong>
+        <span className="units">
+          <a href="/">C</a> | <a href="/">F</a>
+        </span>
+      </div>
+      <div className="col-4 mt-2 text-capitalize">
+        <strong>{weather.description}</strong>
+        <img src={weather.icon} alt={weather.description} width="120px" />
+      </div>
+      <div className="col-4 mt-2">
+        <ul>
+          <li className="list-unstyled">Humidity: {weather.humidity} %</li>
+          <li className="list-unstyled">Wind: {weather.wind} km/h</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+
+  
+
+
+if (content){
     return (
       <div>
         {form}
-        <h1>{city}</h1>
-        <h2>{weather.temperature} °C</h2>
-       
+        {heading}
+       {element}
       </div>
     );
 }else{
-    return form;
+    return (
+      <div>
+        {form}
+        <strong className="text-center">This is your daily forecast, find a city.</strong>
+      </div>
+    );
 }
 }
