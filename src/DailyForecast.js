5304e3bf56fe7c0e14f2ea68e1203a47088c87a0
fonnycode <bf.tinydiaries@gmail.com>
2024-01-21 02:23:53 -0500

    fixed the error on the loop for forecast to simply adding return null

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -16,9 +16,16 @@ export default function DailyForecast(props) {
     return (
       <div className="DailyForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       </div>
     );
